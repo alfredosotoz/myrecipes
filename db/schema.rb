89fdadd49ea3599e54c223f70963378ec297a448
alfredosotoz <alfredosoto@outlook.com>
2016-02-29 23:28:25 +0000

    Add recipes table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229232400) do
+ActiveRecord::Schema.define(version: 20160229232612) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
